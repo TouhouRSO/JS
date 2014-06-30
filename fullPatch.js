5b04f82ad07c5bfbc7083e71a150d6fb7e99fc97
wmichaelv <wmichaelv@hotmail.com>
2014-06-29 20:51:52 -0700

    fullPatch Release

---
+++ fullPatch.js
@@ -0,0 +1,21 @@
+/*******************************************
+ * This is a 3rd party patch for dulst RSO *
+ * Any feedback is appreciated             *
+ *******************************************/
+
+
+/*************
+ * Animation *
+ *************/
+
+$.getScript('https://raw.githubusercontent.com/TouhouRSO/JS/master/battleAnimation/attackInterceptSpell.js');
+
+/*************
+ *    Fix    *
+ *************/
+
+$.getScript('https://raw.githubusercontent.com/TouhouRSO/JS/master/cardDisplay/backCard.js');
+
+
+
+void(0);
