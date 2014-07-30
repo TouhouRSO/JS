a7d772619c1f6c3c3b43372f1ae772865c76fd0f
Michael <wmichaelv@hotmail.com>
2014-07-29 21:34:17 -0700

    Delete attackInterceptSpell.js

--- battleAnimation/attackInterceptSpell.js
+++
@@ -1,5 +0,0 @@
-/******************************************************************************
- * Animation : This script will shift the Attack Intercept Spell to the front *
- ******************************************************************************/
- 
-$("#dulst-customCSS")[0].innerHTML = "div[data-dulstclass~=\"Battle-Spell\"][data-location=\"me-FirstRow\"] {\n    top:540px !important\;\n}\ndiv[data-dulstclass~=\"Battle-Spell\"][data-location=\"opponent-FirstRow\"] {\n    top:380px !important\;\n}"
